005d69edb6543d68b394b03ff57757819d202cd7
sanju <81815695+Sanjuchilukuri@users.noreply.github.com>
2023-07-10 12:12:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stickler Thief/stickler-thief.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int doo(int arr[], int i, int n, vector<int>&dp )
+    {
+        if( i >= n )
+        {
+            return 0;
+        }
+        
+        if( dp[i] != -1 )
+        {
+            return dp[i];
+        }
+        
+        int a = arr[i]+doo(arr,i+2,n,dp);
+        int b = doo(arr,i+1,n,dp);
+        
+        return dp[i] = max(a,b);
+    }
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        vector<int>dp(n+1,-1);
+        return doo(arr,0,n,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
