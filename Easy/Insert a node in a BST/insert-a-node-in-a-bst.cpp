d696fb9734e287dd49279722403ac158dbb66477
sanju <81815695+Sanjuchilukuri@users.noreply.github.com>
2023-09-12 20:48:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert a node in a BST/insert-a-node-in-a-bst.cpp
@@ -0,0 +1,197 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to insert a node in a BST.
+
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    bool available( Node* node, int data )
+    {
+        if( node == NULL )
+        {
+            return false;
+        }
+        
+        if( data > node->data )
+        {
+            return available( node -> right, data );
+        }
+        else if( data == node->data )
+        {
+            return true;
+        }
+        else
+        {
+            return available( node->left, data );
+        }
+    }
+    
+    void dooit( Node* root, int data, char dir, Node* prev )
+    {
+        if( root == NULL )
+        {
+            Node* tmp = new Node(data);
+            if( dir == 'L')
+            {
+                prev->left = tmp;
+            }
+            else
+            {
+                prev->right = tmp;
+            }
+            return;
+        }
+        
+        if( data > root->data )
+        {
+            dooit( root->right, data, 'R', root );
+        }
+        else
+        {
+            dooit( root->left, data, 'L', root);
+        }
+    }
+    Node* insert(Node* node, int data) 
+    {
+        // Your code goes here
+        if( available( node, data ) )
+        {
+            return node;
+        }
+        else
+        {
+            dooit(node,data,'  ',NULL);
+        }
+        
+        return node;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void inorder(Node* root, vector<int>& v) {
+    if (root == NULL) return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        getline(cin, s);
+        int k = stoi(s);
+        // getline(cin, s);
+        Solution ob;
+        ob.insert(root, k);
+        vector<int> v;
+        inorder(root, v);
+        for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+        cout << endl;
+
+        // cout<<"~"<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
